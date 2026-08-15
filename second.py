d172e00d85d6772452fff2ec9452c51493aebaf1
Vivek chahal <vivekchahal9520635797@gmail.com>
2026-08-15 13:12:59 +0530

    updated second file

--- second.py
+++ second.py
@@ -22,3 +22,36 @@ if result != -1:
     print("found at index:", result)
 else:
     print("not found")
+
+
+def paper_fint(name,paper):
+    for paper in paper:
+        if paper==name:
+            return True
+    return False
+
+paper=["John", "Alice", "Bob", "Charlie", "David"]
+
+search_name = "Alice"
+
+result = paper_fint(search_name, paper)
+if result:
+    print("found")
+else:
+    print("not found")
+
+#                           Duplicate value
+
+def duplicate_value(number):
+    for i in range(len(number)):
+        for j in range(i + 1, len(number)):
+            if i !=j and number[i] == number[j]:
+                return True
+    return False
+number=[1,2,3,4,5,6,7,8,9,10]
+
+result=duplicate_value(number)
+if result:
+    print("duplicate value found")
+else:
+    print("no duplicassste value found")

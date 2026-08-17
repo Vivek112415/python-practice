ac0fbe3ae09f7ca44fe5b1fc4a8a504d49b9cb95
Vivek chahal <vivekchahal9520635797@gmail.com>
2026-08-17 22:57:08 +0530

    update fourth.py

--- fourth.py
+++ fourth.py
@@ -7,3 +7,10 @@ for i in range(1,11):
 
 
                                     # wap to create a table using while loop
+
+n= int(input("enter a table number :"))
+
+i =1
+while i<=10:
+    print(n*i)
+    i=i+1

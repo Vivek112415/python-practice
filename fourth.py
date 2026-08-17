05d17e9ede1cc154511caee5427e8d087da574f8
Vivek chahal <vivekchahal9520635797@gmail.com>
2026-08-17 22:45:37 +0530

    fourth.py

---
+++ fourth.py
@@ -0,0 +1,9 @@
+                                # wap to create a table
+
+n = int(input("enter a table number :"))
+for i in range(1,11):
+    print(n,"*",i,":",n*i)
+
+
+
+                                    # wap to create a table using while loop

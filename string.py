2b96d1904a18703db3843ffd4a72951d862d6f96
Vivek chahal <vivekchahal9520635797@gmail.com>
2026-08-18 19:55:51 +0530

    string.py

---
+++ string.py
@@ -0,0 +1,36 @@
+                                    # Creating a list
+
+fruits = ["Apple", "Banana", "Mango", "Orange", "Grapes"]
+
+print("Original List:", fruits)
+
+# Accessing
+print("Accessing:", fruits[0])
+
+# Slicing
+print("Slicing:", fruits[1:4])
+
+# Modifying
+fruits[2] = "Pineapple"
+print("Modifying:", fruits)
+
+# Adding
+fruits.append("Watermelon")
+print("Adding:", fruits)
+
+# Removing
+fruits.remove("Banana")
+print("Removing:", fruits)
+
+# Sorting
+fruits.sort()
+print("Sorting:", fruits)
+
+# Reversing
+fruits.reverse()
+print("Reversing:", fruits)
+
+# Finding Length
+print("Length:", len(fruits))
+
+

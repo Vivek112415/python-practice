53652c4c509d706acddc0c1a59f92fbe116798d6
Vivek chahal <vivekchahal9520635797@gmail.com>
2026-08-21 10:01:28 +0530

    six.py

---
+++ six.py
@@ -0,0 +1,28 @@
+
+
+                        # control Statement if and else
+                    
+                    # user voter adulat and not adulat
+
+n=int(input("Enter the age :"))
+
+if n>=18:
+    print("Adulat")
+else:
+    print("Not Adulat")
+
+
+                            # WAP to check a number grade if and elif use
+
+num=int(input("Enter the number :"))
+
+if num>=90:
+    print("Grade : A")
+elif num>=80:
+    print("Grae : B")
+elif num>=70:
+    print("Grade : c")
+elif num>=60:
+    print("Grade : D")
+else:
+    print("Fail")

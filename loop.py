a0aec1e3eb6165d55deb1aee19a688a383886231
Vivek chahal <vivekchahal9520635797@gmail.com>
2026-08-21 11:39:44 +0530

    six.py

---
+++ loop.py
@@ -0,0 +1,28 @@
+
+                # For loop 
+
+        # WAP to use list print
+
+# list=["C","C++","Java","Python","SQL"]
+
+# for i in list:
+#     print(i)
+
+
+#                   WAP to use a list print while loop
+
+# li=["c++","Java","PL/SQL","Science"]
+# i=0
+# while i<=4:
+#     print(li[i])
+#     i=i+1
+
+
+
+# for i in range(5):
+#     print("Chahal")
+
+n=input("enter a name you cha :")
+
+for i in range(len(n)):
+    print(n[i])

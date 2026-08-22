5662bee503d69677f5abe19fa31e3e9e3118827b
Vivek chahal <vivekchahal9520635797@gmail.com>
2026-08-22 21:55:49 +0530

    update pattren.py

--- pattren.py
+++ pattren.py
@@ -29,3 +29,18 @@ for i in range(1,5):
     for j in range(1,5):
         print("*",end=" ")
     print()
+
+
+"""
+5, 4, 3, 2, 1
+4, 3, 2, 1
+3, 2, 1
+2, 1
+1
+"""
+n = 5
+for i in range(n,0,-1):
+    for j in range(i,0,-1):
+        print(j,end=" ")
+    print()
+    

0d0d21262d816859235d0ad1377d2db0cbac3cd2
Vivek chahal <vivekchahal9520635797@gmail.com>
2026-08-23 22:02:34 +0530

    update list.py

--- list.py
+++ list.py
@@ -8,3 +8,8 @@ print("min list element is:",a)
 list1=[11,22,4,6,4,7]
 b=max(list1)
 print("max list element is:",b)
+
+                        # apend list element
+list2=[11,22,4,6,4,7]
+list2.append(99)
+print("append list element is:",list2)

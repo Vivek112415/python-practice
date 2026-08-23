2978ab42a472a7c864077eef99d8f487ee4b9253
Vivek chahal <vivekchahal9520635797@gmail.com>
2026-08-23 17:19:48 +0530

    update function.py

--- function.py
+++ function.py
@@ -1,39 +1,50 @@
 
 
-                        # even odd to list numbere
+#                         # even odd to list numbere
 
-def cal_add(list):
-    list=[1,22,43,55,22,11,33,13,7,56,96,44,5,3,6]
-    odd=[]
-    even=[]
-    for i in list:
-        if i % 2==0:
-            even.append(i)
-        else:
-            odd.append(i)
-    print("oringial list:",list)
-    print("even number list:",even)
-    print("odd number list:",odd)
+# def cal_add(list):
+#     list=[1,22,43,55,22,11,33,13,7,56,96,44,5,3,6]
+#     odd=[]
+#     even=[]
+#     for i in list:
+#         if i % 2==0:
+#             even.append(i)
+#         else:
+#             odd.append(i)
+#     print("oringial list:",list)
+#     print("even number list:",even)
+#     print("odd number list:",odd)
 
-cal_add(list)
+# cal_add(list)
 
-                        # user input to list number
+#                         # user input to list number
 
-def cal_add(list):
-    list=[]
-    n=int(input("enter the number of elements:"))
-    for i in range(10):
-        ele=int(input("enter the element:"))
-        list.append(ele)
-    odd=[]
-    even=[]
-    for i in list:
-        if i % 2==0:
-            even.append(i)
-        else:
-            odd.append(i)
-    print("oringial list:",list)
-    print("even number list:",even)
-    print("odd number list:",odd)
+# def cal_add(list):
+#     list=[]
+#     n=int(input("enter the number of elements:"))
+#     for i in range(10):
+#         ele=int(input("enter the element:"))
+#         list.append(ele)
+#     odd=[]
+#     even=[]
+#     for i in list:
+#         if i % 2==0:
+#             even.append(i)
+#         else:
+#             odd.append(i)
+#     print("oringial list:",list)
+#     print("even number list:",even)
+#     print("odd number list:",odd)
 
-cal_add(list)
+# cal_add(list)
+
+
+                        # Recursion to function
+
+def fact(n):
+    if n==1:
+        return 1
+    else:
+        return n*fact(n-1)
+
+print("Factorial of number is:",fact(5))

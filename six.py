c7e913a6a962b9d0f1b3f28047dec4d6e56b05c9
Vivek chahal <vivekchahal9520635797@gmail.com>
2026-08-24 18:30:49 +0530

    update six.py

--- six.py
+++ six.py
@@ -4,49 +4,57 @@
                     
                     # user voter adulat and not adulat
 
-n=int(input("Enter the age :"))
+# n=int(input("Enter the age :"))
 
-if n>=18:
-    print("Adulat")
-else:
-    print("Not Adulat")
+# if n>=18:
+#     print("Adulat")
+# else:
+#     print("Not Adulat")
 
 
                             # WAP to check a number grade if and elif use
 
-num=int(input("Enter the number :"))
+# num=int(input("Enter the number :"))
 
-if num>=90:
-    print("Grade : A")
-elif num>=80:
-    print("Grae : B")
-elif num>=70:
-    print("Grade : c")
-elif num>=60:
-    print("Grade : D")
-else:
-    print("Fail")
+# if num>=90:
+#     print("Grade : A")
+# elif num>=80:
+#     print("Grae : B")
+# elif num>=70:
+#     print("Grade : c")
+# elif num>=60:
+#     print("Grade : D")
+# else:
+#     print("Fail")
 
 
                             # WAP to chek number even or odd
 
-a=int(input("Enter a number :"))
+# a=int(input("Enter a number :"))
 
-if a%2==0:
-    print("Even Number ")
-else:
-    print("Odd Number")
+# if a%2==0:
+#     print("Even Number ")
+# else:
+#     print("Odd Number")
 
 
 
                                 # WAP to traffic light a sinlge program
 
 
-light =input("Enter a light :")
+# light =input("Enter a light :")
 
-if light=="Green":
-    print("GO")
-elif light=="yellow":
-    print("start")
-else:
-    print("Stop")
+# if light=="Green":
+#     print("GO")
+# elif light=="yellow":
+#     print("start")
+# else:
+#     print("Stop")
+
+
+
+        # circle radese
+
+n=int(input("Enter a number :"))
+area =3.14*n*n
+print(area)

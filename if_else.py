96557f5720ab626e0dbcf44df3eeb2f142f124e6
Vivek chahal <vivekchahal9520635797@gmail.com>
2026-08-25 09:08:21 +0530

    update if_else.py

--- if_else.py
+++ if_else.py
@@ -15,13 +15,26 @@
 
 # Write a Python program to find the largest of three numbers
 
-a=5
-b=7
-c=3
+# a=5
+# b=7
+# c=3
 
-if a>b & a>c:
-    print("Largest",a)
-elif b>a & b>c:
-    print("Largest",b)
+# if a>b & a>c:
+#     print("Largest",a)
+# elif b>a & b>c:
+#     print("Largest",b)
+# else:
+#     print("Largest",c)
+
+# user input largest number
+
+n=int(input("enter a number :"))
+m=int(input("enter a number :"))
+o=int(input("enter a number :"))
+
+if n>m & n>0:
+    print("Largest number n :",n)
+elif m>0 & m>0:
+    print("Largest numver M :",m)
 else:
-    print("Largest",c)
+    print("Largest o:",o)

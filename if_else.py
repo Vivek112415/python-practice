0d092ef4f11b3a0115153d177bfa2b8315036b6f
Vivek chahal <vivekchahal9520635797@gmail.com>
2026-08-25 08:33:14 +0530

    update if_else.py

--- if_else.py
+++ if_else.py
@@ -1,14 +1,27 @@
 
 # write a program to grade
 
-n=int(input("enter a number :"))
-if n>=90:
-    print("Grade : A")
-elif n>=80:
-    print("Grade : B")
-elif n>=70:
-    print("Grade : C")
-elif n>=60:
-    print("Grade : D")
+# n=int(input("enter a number :"))
+# if n>=90:
+#     print("Grade : A")
+# elif n>=80:
+#     print("Grade : B")
+# elif n>=70:
+#     print("Grade : C")
+# elif n>=60:
+#     print("Grade : D")
+# else:
+#     print("Fail")
+
+# Write a Python program to find the largest of three numbers
+
+a=5
+b=7
+c=3
+
+if a>b & a>c:
+    print("Largest",a)
+elif b>a & b>c:
+    print("Largest",b)
 else:
-    print("Fail")
+    print("Largest",c)

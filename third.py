a691952b1757b33fed03108370abb2f7a0d8a662
Vivek chahal <vivekchahal9520635797@gmail.com>
2026-08-26 10:02:20 +0530

    update third.py

--- third.py
+++ third.py
@@ -1,16 +1,29 @@
                     # for loop to find factorial of a number
-num =int(input("enter the number :"))
-fact =1
-for i in range(1,num+1):
-    fact = fact*i
-    print(fact)
+# num =int(input("enter the number :"))
+# fact =1
+# for i in range(1,num+1):
+#     fact = fact*i
+#     print(fact)
 
                     # while loop to find factorial of a number 
 
-num =int(input("enter the number :"))
-fact=1
-i =1
-while i<num+1:
-    fact = fact*i
-    print(fact)
-    i+=1
+# num =int(input("enter the number :"))
+# fact=1
+# i =1
+# while i<num+1:
+#     fact = fact*i
+#     print(fact)
+#     i+=1
+
+
+a=int(input("Enter a firt number :"))
+b=int(input("Enter a second number :"))
+
+add=a+b
+sub=a-b
+multip=a*b
+divided=a/b
+print("add :",add)
+print("subtract :",sub)
+print("divided:",divided)
+print("multipleye :",multip)

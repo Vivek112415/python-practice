247e9c2d59ce8b34081b9f1d52e108559d3ca794
Vivek chahal <vivekchahal9520635797@gmail.com>
2026-08-27 10:35:42 +0530

    update first.py

--- first.py
+++ first.py
@@ -1,20 +1,24 @@
-print("chahal")
+# print("chahal")
 
-for i in range(5):
-    print(i)
+# for i in range(5):
+#     print(i)
 
-print("This is a test script.")
+# print("This is a test script.")
 
-a=5
-b=10
-sum=a+b
-print(sum)
+# a=5
+# b=10
+# sum=a+b
+# print(sum)
 
-n =int(input("Enter a number: "))
-for i in range(1,11):
-    print(i,"*",n,"=",i*n)
+# n =int(input("Enter a number: "))
+# for i in range(1,11):
+#     print(i,"*",n,"=",i*n)
 
-i =1 
-while i<=10:
-    print(i*5)
-    i+=1
+# i =1 
+# while i<=10:
+#     print(i*5)
+#     i+=1
+
+
+str="python programming"
+print(type(str))

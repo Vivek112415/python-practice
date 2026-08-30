609448f5415659f5b46922fc82a1a36e1736ea5d
Vivek chahal <vivekchahal9520635797@gmail.com>
2026-08-30 18:47:20 +0530

    update first.py

--- first.py
+++ first.py
@@ -29,9 +29,23 @@
 
 # Find the largest of two numbers.
 
-num1=int(input("Enter a number :"))
-num2=int(input("Enter a number :"))
-if num1 > num2:
-    print("largest number is  :",num1)
-else:
-    print("largest number is :",num2)
+# num1=int(input("Enter a number :"))
+# num2=int(input("Enter a number :"))
+# if num1 > num2:
+#     print("largest number is  :",num1)
+# else:
+#     print("largest number is :",num2)
+
+
+# swap two number
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+temp = num1
+num1 = num2
+num2 = temp
+
+print("After swapping:")
+print("First number:", num1)
+print("Second number:", num2)
